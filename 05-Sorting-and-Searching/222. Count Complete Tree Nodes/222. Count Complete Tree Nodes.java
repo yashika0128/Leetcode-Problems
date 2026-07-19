82c4d2acdf5ac260d3216d653076f25d96ebd5e9
Yashika Chaubey <chaubeyyashika3@gmail.com>
2026-07-19 17:28:57 +0530

    [222] [Time Beats: 100.00%] [Memory Beats: 84.97%] - Leet2Hub

---
+++ 05-Sorting-and-Searching/222. Count Complete Tree Nodes/222. Count Complete Tree Nodes.java
@@ -0,0 +1,22 @@
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int countNodes(TreeNode root) {
+18        if(root == null) return 0;
+19
+20        return 1 + countNodes(root.left) + countNodes(root.right);
+21    }
+22}
